93ef760f7c78040448945b97417c024c5c46a929
maqiang <maqiang@kd.cn>
2019-10-15 09:33:48 +0800

    spring cloud config

--- fd-modules/pig-upms-service/src/main/java/com/github/fd/admin/FdAdminApplication.java
+++ fd-modules/pig-upms-service/src/main/java/com/github/fd/admin/FdAdminApplication.java
@@ -2,13 +2,16 @@ package com.github.fd.admin;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author mq
  */
-@MapperScan(basePackages = "com.github.fd.admin.mapper")
+@MapperScan("com.github.fd.admin.mapper")
 @EnableEurekaClient
 @SpringBootApplication
 public class FdAdminApplication {
