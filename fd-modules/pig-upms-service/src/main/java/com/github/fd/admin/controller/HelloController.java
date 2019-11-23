6b2d383129e79c960a4e8a6ed326a7f4a03504bb
maqiang <maqiang@kd.cn>
2019-11-23 14:42:10 +0800

    logstash配置

--- fd-modules/pig-upms-service/src/main/java/com/github/fd/admin/controller/HelloController.java
+++ fd-modules/pig-upms-service/src/main/java/com/github/fd/admin/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.github.fd.admin.controller;
 
 import com.github.fd.admin.model.entity.SysDept;
 import com.github.fd.admin.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import java.util.Date;
 /**
  * @author mq
  */
+@Slf4j
 @RestController
 @RequestMapping("dept")
 public class HelloController {
@@ -27,6 +29,9 @@ public class HelloController {
         sysDept.setDelFlag("0");
         sysDept.setCreateTime(new Date());
         sysDept.setUpdateTime(new Date());
+        log.info("添加数据{}", sysDept.toString());
+        log.warn("添加数据{}", sysDept.toString());
+        log.error("添加数据{}", sysDept.toString());
         deptService.insert(sysDept);
         return sysDept.getDeptId();
     }
